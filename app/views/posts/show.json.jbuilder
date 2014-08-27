0210b5ba3c1197f254da0cf921531478d77c512d
Prelang Builder <builder@prelang.com>
2014-08-27 09:03:18 +0000

    Generated scaffolding for model 'Post' via Rails

---
+++ app/views/posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post, :id, :user_id, :title, :content, :published, :nbr_views, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts
+
   resources :comments
 
   resources :categorie_audios
