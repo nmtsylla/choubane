d5fe789e4ebbe91a4a961741d84102c36f498492
Prelang Builder <builder@prelang.com>
2014-08-27 09:03:05 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/categorie_audios_controller.rb
+++ app/controllers/categorie_audios_controller.rb
@@ -1,4 +1,8 @@
 class CategorieAudiosController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_categorie_audio, only: [:show, :edit, :update, :destroy]
 
   # GET /categorie_audios
